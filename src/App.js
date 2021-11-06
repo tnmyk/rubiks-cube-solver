83bf2549265d7839a852595d0c6e112f4d7b4951
Tanmaykk <tanmaykachroo@gmail.com>
2021-11-06 19:31:23 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <>working</>;
-}
+import "./css/global.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
