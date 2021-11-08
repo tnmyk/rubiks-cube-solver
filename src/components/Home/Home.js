614ea8de4fd5912edf37f605824408e1455990ec
Tanmaykk <tanmaykachroo@gmail.com>
2021-11-08 21:35:04 +0530

    home changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <>HOME HAHA</>;
+  return (
+    <>
+      <Link
+        to="/solve"
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%,-50%)",
+          color: "white",
+          padding: "1rem",
+          
+          backgroundColor: "black",
+        }}
+      >
+        Go to solver
+      </Link>
+    </>
+  );
 };
 
 export default Home;
